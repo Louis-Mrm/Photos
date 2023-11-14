c58ea96529a4729da22c587dfcb2f4a3f75371b3
Louis-Mrm <147149049+Louis-Mrm@users.noreply.github.com>
2023-11-14 17:08:07 +0100

    Add files via upload

---
+++ storage/framework/views/0ad9459be51aae72d4c189f6ad24e267.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Album</title>
+</head>
+<body>
+    <h1>Bienvenue !</h1>
+    <a href="<?php echo e(route('galerie')); ?>">Accéder à la galerie</a>
+</body>
+</html><?php /**PATH C:\Users\screv\Desktop\cours_iut\TPPhotos\photos\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/33f89d03acc169e79498b4adfa98bf76.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Galerie</title>
+</head>
+<body>
+    
+</body>
+</html><?php /**PATH C:\Users\screv\Desktop\cours_iut\TPPhotos\photos\resources\views/galerie.blade.php ENDPATH**/ ?>
